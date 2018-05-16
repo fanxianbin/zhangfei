e3e60b5f6e85d1184acc596902173f6bf89ceaa6
fanxianbin <1605411197@qq.com>
2018-05-16 13:12:26 +0800

    删除用户信息

---
+++ src/main/java/transaction/RemoveUser.java
@@ -0,0 +1,10 @@
+package transaction;
+
+/**
+ * Created by fanxianb on 2018/5/16.
+ */
+public class RemoveUser {
+    public static void main(String[] args) {
+        String name = new String("");
+    }
+}
